8d28f2b1ebacbba5de13acd2b597b1b13bcfbc3c
oskca <datago@yeah.net>
2016-03-27 19:36:42 +0800

    update documentation

--- vue.go
+++ vue.go
@@ -1,4 +1,5 @@
-// Package vue provides gopherjs bindings for VueJS
+// Package vue provides gopherjs bindings for VueJS. see func `New` for
+//  futher information
 package vue
 
 import (
@@ -16,33 +17,34 @@ type Vue struct {
 	// vm.$data
 	// 	Type: Object
 	// Details:
-	// 		The data object that the Vue instance is observing.
-	//  	You can swap it with a new object.
-	// 		The Vue instance proxies access to the properties on its data object.
+	//  The data object that the Vue instance is observing.
+	//  You can swap it with a new object.
+	//  The Vue instance proxies access to the properties on its data object.
 	Data *js.Object `js:"$data"`
 
 	// vm.$el
 	//  Type: HTMLElement
 	//  Read only
 	// Details:
-	//  	The DOM element that the Vue instance is managing.
-	// 		Note that for Fragment Instances, vm.$el will return an anchor node that
-	// 		indicates the starting position of the fragment.
+	//  The DOM element that the Vue instance is managing.
+	//	Note that for Fragment Instances, vm.$el will
+	//  return an anchor node that
+	//	indicates the starting position of the fragment.
 	El *js.Object `js:"$el"`
 
 	// vm.$options
 	//  Type: Object
 	//  Read only
 	// Details:
-	//    The instantiation options used for the current Vue instance.
-	//    This is useful when you want to include custom properties in the options:
-	//
-	// 	  new Vue({
-	// 	    customOption: 'foo',
-	// 	    created: function () {
-	// 	      console.log(this.$options.customOption) // -> 'foo'
-	// 	    }
-	// 	  })
+	//  The instantiation options used for the current Vue instance.
+	//  This is useful when you want to include custom
+	//  properties in the options:
+	//	 new Vue({
+	//	   customOption: 'foo',
+	//	   created: function () {
+	//	     console.log(this.$options.customOption) // -> 'foo'
+	//	   }
+	//	 })
 	Options *js.Object `js:"$options"`
 
 	// vm.$parent
@@ -108,7 +110,8 @@ type Vue struct {
 	// vm.$get( expression )
 	// 	expression String
 	// Retrieve a value from the Vue instance given an expression.
-	// Expressions that throw errors will be suppressed and return undefined.
+	// Expressions that throw errors will be suppressed
+	// and return undefined.
 	Get func(expression string) *js.Object `js:"$get"`
 
 	// vm.$set( keypath, value )
@@ -136,7 +139,8 @@ type Vue struct {
 
 	// vm.$interpolate( templateString )
 	// 	templateString String
-	// Evaluate a piece of template string containing mustache interpolations.
+	// Evaluate a piece of template string containing
+	// mustache interpolations.
 	// Note that this method simply performs string interpolation;
 	// attribute directives are not compiled.
 	//
@@ -154,8 +158,9 @@ type Vue struct {
 	//	event String
 	// 	args… optional
 	//
-	// Dispatch an event from the current vm that propagates all the way up to its $root.
-	// If a callback returns false, it will stop the propagation at its owner instance.
+	// Dispatch an event from the current vm that propagates
+	// all the way up to its $root. If a callback returns false,
+	// it will stop the propagation at its owner instance.
 	Dispatch func(event string, args ...interface{}) `js:"$dispatch"`
 
 	// vm.$broadcast( event, [args…] )
@@ -195,13 +200,15 @@ type Vue struct {
 	//
 	// If no arguments are given, stop listening for all events;
 	// if only the event is given, remove all callbacks for that event;
-	// if both event and callback are given, remove that specific callback only.
+	// if both event and callback are given,
+	// remove that specific callback only.
 	Off func(event ...string) `js:"$off"`
 
 	// DOM
 	// All vm DOM manipulation methods work like their jQuery counterparts -
 	// except they also trigger Vue.js transitions if there are any declared
-	// on vm’s $el. For more details on transitions see Adding Transition Effects.
+	// on vm’s $el. For more details on transitions
+	// see Adding Transition Effects.
 
 	// vm.$appendTo( element|selector, [callback] )
 	// 	element HTMLElement | selector String
@@ -241,8 +248,9 @@ type Vue struct {
 	Mount func(elementOrselector string) *Vue `js:"$mount"`
 
 	// vm.$destroy( [remove] )
-	// remove Boolean optional
-	// Completely destroy a vm. Clean up its connections with other existing vms,
+	//  remove Boolean optional
+	// Completely destroy a vm.
+	// Clean up its connections with other existing vms,
 	// unbind all its directives and remove its $el from the DOM.
 	// Also, all $on and $watch listeners will be automatically removed.
 	Destroy func(remove bool) `js:"$destroy"`
@@ -251,22 +259,26 @@ type Vue struct {
 	// 	element HTMLElement
 	// Partially compile a piece of DOM (Element or DocumentFragment).
 	// The method returns a decompile function that tearsdown the directives
-	// created during the process. Note the decompile function does not remove
-	// the DOM. This method is exposed primarily for writing advanced custom directives.
+	// created during the process.
+	// Note the decompile function does not remove the DOM.
+	// This method is exposed primarily for
+	// writing advanced custom directives.
 	Compile func(element string) `js:"$compile"`
 
 	// vm.$addChild( [options, constructor] )
-	// options Object optional
-	// constructor Function optional
+	//  options Object optional
+	//  constructor Function optional
 	//
 	// Adds a child instance to the current instance.
 	// The options object is the same in manually instantiating an instance.
 	// Optionally you can pass in a constructor created from Vue.extend().
 	//
-	// There are three implications of a parent-child relationship between instances:
-	// The parent and child can communicate via the event system.
-	// The child has access to all parent assets (e.g. custom directives).
-	// The child, if inheriting parent scope, has access to parent scope data properties.
+	// There are three implications of
+	// a parent-child relationship between instances:
+	//  The parent and child can communicate via the event system.
+	//  The child has access to all parent assets (e.g. custom directives).
+	//  The child, if inheriting parent scope,
+	//   has access to parent scope data properties.
 	AddChild func(options js.M) `js:"$addChild"`
 
 	// vm.$log( [keypath] )
@@ -296,9 +308,7 @@ type Vue struct {
 //  and `exported fields` should have proper `js struct tag` for
 //  bidirectionaly data bindings
 //
-/////////////// ONE IMPORTANT NOTICE `struct exported funcs` ///////////////////
-//
-// Rules for exported functions usage:
+// Rules for exported functions usage IMPORTANT!:
 //
 //  * If your func uses any of the `exported fields`, then DONOT modify any.
 //  These can be viewed roughly as `computed attribute` in `VueJS` wourld,
