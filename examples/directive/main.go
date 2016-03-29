5ff286e1e27847ac04c92884d1cfa52fba488ab4
oskca <datago@yeah.net>
2016-03-29 11:08:30 +0800

    update

--- examples/directive/main.go
+++ examples/directive/main.go
@@ -3,28 +3,27 @@ package main
 import (
 	"github.com/gopherjs/gopherjs/js"
 	"github.com/oskca/gopherjs-vue"
-	"github.com/oskca/gopherjs-vue/directive"
+	"time"
 )
 
 type Model struct {
 	*js.Object
 	Text string `js:"Text"`
+	Time string `js:"Time"`
 }
 
 func main() {
-	d := directive.New("myd")
-	d.SetUpdater(func(ctx *directive.Context, val *js.Object) {
+	d := vue.NewDirective()
+	d.SetUpdater(func(ctx *vue.DirectiveContext, val *js.Object) {
 		println("directive name:", ctx.Name)
 		println("directive exp:", ctx.Expression)
 		println("directive values:", val)
-	}).Register()
+	}).Register("myd")
 
 	m := &Model{
 		Object: js.Global.Get("Object").New(),
 	}
 	m.Text = "a string"
+	m.Time = time.Now().String()
 	vue.New("body", m)
-	m2 := &Model{
-		Object: js.Global.Get("Object").New(),
-	}
 }
