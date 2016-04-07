a6bdef9bed05e766d0c81da25918fe55d6c6e1a4
oskca <datago@yeah.net>
2016-04-07 09:28:25 +0800

    using interface{} in Filter to utilize GopherJS type convertion automatically

--- filter.go
+++ filter.go
@@ -8,15 +8,14 @@ type Filter struct {
 	*js.Object
 	Read  interface{} `js:"read"`
 	Write interface{} `js:"write"`
-	// Read  func(vm *ViewModel, args []*js.Object) *js.Object `js:"read"`
-	// Write func(vm *ViewModel, args []*js.Object) *js.Object `js:"write"`
 }
 
-func NewFilter(read func(val *js.Object) *js.Object) *Filter {
+// using interface{} type here to utilize GopherJS type convertion automatically
+func NewFilter(readerfn interface{}) *Filter {
 	f := &Filter{
 		Object: js.Global.Get("Object").New(),
 	}
-	f.Read = read
+	f.Read = readerfn
 	return f
 }
 
